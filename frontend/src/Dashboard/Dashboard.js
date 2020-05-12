b409c73d3d75b02ec8e9df643878f00867bb998e
Kewei Yan <yankw62@gmail.com>
2020-05-12 04:33:26 -0400

    upload and compile updated

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Button, Select} from 'antd';
+import {Button, Select, Upload} from 'antd';
 import 'antd/dist/antd.css';
 import './Dashboard.css';
-import { ToolOutlined, UploadOutlined, RotateRightOutlined, SaveOutlined, HomeOutlined} from '@ant-design/icons'
+import { ToolOutlined, UploadOutlined, RotateRightOutlined, SaveOutlined, HomeOutlined} from '@ant-design/icons';
+import axios from 'axios';
+import Img from 'react-image';
 
 const Option = Select.Option;
 
@@ -16,11 +18,18 @@ children.push(<Option key="5">RUNTIME:LLVM</Option>);
 children.push(<Option key="6">RUNTIME:GOMP</Option>);
 children.push(<Option key="7">LIB:MPI</Option>);
 children.push(<Option key="8">LIB:MKL</Option>);
-
+              
 class Dashboard extends React.Component {
     state = {
         myDisplay: 'row',
-        myWidth: 0
+        myWidth: 0,
+        myFile: null,
+        myPreview: null,
+        myCompiler: 'GCC',
+        myRuntime: 'LLVM',
+        myLib: null,
+        myStatus: null,
+        myOutput: null
     };
     
     constructor(props) {
@@ -55,7 +64,50 @@ class Dashboard extends React.Component {
     screenChange = () => {
         window.addEventListener('resize', this.resize);
     }
-              
+
+/*
+ 
+ The desired function call could be like:
+ 
+ def func():
+    return { "status":"...", "msg": "..." }
+ 
+ status could be SUCCESS or FAILED; msg could be the url of AST or error message
+ 
+ In the demo for now, I have:
+ 
+ {
+   "msg": "...",
+   "status": "..."
+ }
+
+Of course, CORS is needed
+
+ */
+    Onchg = (info) => {
+        this.setState({myFile: info.file.name});
+        
+        const tmp = new FileReader();
+        
+        tmp.readAsText(info.file.originFileObj); // This is only part of Blob!
+        tmp.onload = (res) => {
+              this.setState({myPreview: res.target.result});
+        }
+
+        console.log(info.file.originFileObj);
+
+        console.log(this.state.myFile);
+    };
+
+    Cpl = () => {
+       axios.get('http://0.0.0.0:5000/').then(res => {
+          this.setState({ myStatus: res.data.status, myOutput: res.data.msg});
+       })
+
+       console.log(this.state.myStatus);
+       console.log(this.state.myOutput);
+    };
+
     Onclk = () => {
        if (this.state.myDisplay == 'row') {
            this.setState({myDisplay: 'column'});
@@ -65,8 +117,16 @@ class Dashboard extends React.Component {
 
        console.log('display changed!');
     };
-              
+
     render() {
+        let OUTPUT;
+
+        if (this.state.myStatus == "SUCCESS!") {
+              OUTPUT = <Img className = "outputbox" src = {this.state.myOutput}/>
+        } else {
+              OUTPUT = <textarea className = "outputbox" placeholder="Results/Error messages will be shown here" value = {this.state.myOutput}/>
+        }
+
         return (
             <div>
                 <div className = "logo">
@@ -74,10 +134,10 @@ class Dashboard extends React.Component {
                 </div>
                 <div className = "opts">
                     <Button><HomeOutlined /></Button>
-                    <Button><UploadOutlined /></Button>
+                    <Upload onChange = {(info) => this.Onchg(info)} showUploadList = {false} ><Button><UploadOutlined /></Button></Upload>
                     <Button><SaveOutlined /></Button>
-                    <Button onClick = {() => this.Onclk()} ><RotateRightOutlined /></Button>
-                    <Button type="primary">Compile</Button>
+                    <Button onClick = {() => this.Onclk()}><RotateRightOutlined /></Button>
+                    <Button type="primary" onClick = {() => this.Cpl()}>Compile</Button>
                 </div>
                 <div className = "tags">
                     <Select
@@ -90,8 +150,8 @@ class Dashboard extends React.Component {
                     </Select>
                 </div>
                 <div className = "texts" style = {{flexDirection: this.state.myDisplay}}>
-                    <textarea className = "inputbox" placeholder="Type or upload your code"/>
-                    <textarea className = "outputbox" placeholder="Results/Error messages will be shown here"/>
+                    <textarea className = "inputbox" placeholder="Type or upload your code" value = {this.state.myPreview}/>
+                    { OUTPUT }
                 </div>
             </div>
         )
